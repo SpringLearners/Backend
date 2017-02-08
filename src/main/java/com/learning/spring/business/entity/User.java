4dcf4929aec5be7f1fbcedcac9fec4558076135f
MCzarnecki <michal.boguslaw.czarnecki@gmail.com>
2017-02-08 15:40:14 +0100

    Update User.java

--- src/main/java/com/learning/spring/business/entity/User.java
+++ src/main/java/com/learning/spring/business/entity/User.java
@@ -3,9 +3,13 @@ package com.learning.spring.business.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+//TODO: entity nie będzie w pakiecie biznesowym, będziemy je po prostu w entity
 /**
  * Created by Beata Kalis on 2017-02-08.
  */
+
+//TODO: wiem że Lombok sporo ułatwia, ale raczej się nie spotkałem by się go używało, a niektóre technologie się podejrzewam (jak np. JSF z nim nie współpracują bo działą bezpośrednio podcza kompilacj)
+//TODO: osobiście wolałbym go nie używać ale przedyskutujemy to :) 
 @Data
 @AllArgsConstructor
 public class User {
