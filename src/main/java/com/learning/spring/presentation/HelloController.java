a9e5c677af8c0aa9cc1a06463cc094fed9fb9d92
Beata <beatka1102@wp.pl>
2017-02-08 23:10:00 +0100

    First task - refactor

--- src/main/java/com/learning/spring/presentation/HelloController.java
+++ src/main/java/com/learning/spring/presentation/HelloController.java
@@ -2,22 +2,17 @@ package com.learning.spring.presentation;
 
 import org.springframework.web.bind.annotation.*;
 
-// TODO: Nie wrzucamy takich podpisów, usuwaj je
-/**
- * Created by Beata Kalis on 2017-02-06.
- */
-
-//TODO: Pomyśl czy nie da się czegoś w tej klasie ulepszyć, może @Request Mapping? :) 
 @RestController
+@RequestMapping(value = "/hello")
 public class HelloController {
 
-    @RequestMapping(value = "/hello/greetings", method = RequestMethod.GET)
+    @RequestMapping(value = "/greetings", method = RequestMethod.GET)
     public String method(@RequestParam(value="name", defaultValue = "World", required = false) String name,
                          @RequestParam(value = "pseudo", defaultValue = "", required = false) String pseudo) {
         return "Hello " + name + " " + pseudo;
     }
 
-    @RequestMapping(value = "/hello/post", method = RequestMethod.POST)
+    @RequestMapping(value = "/post", method = RequestMethod.POST)
     public String answerPost(@RequestBody String name) {
         return "Hello " + name;
     }
--- src/main/java/com/learning/spring/presentation/UserController.java
+++ src/main/java/com/learning/spring/presentation/UserController.java
@@ -1,14 +1,11 @@
 package com.learning.spring.presentation;
 
-import com.learning.spring.business.entity.User;
+import com.learning.spring.entity.User;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Created by Beata Kalis on 2017-02-08.
- */
 @RestController
 public class UserController {
 
